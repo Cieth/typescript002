dcd7368fb8bcca95af9d6a1a7cfb4ffa4a749f42
Omar Vega <omarvega598@gmail.com>
2022-10-18 23:27:47 -0500

    add stuff

--- src/index.tsx
+++ src/index.tsx
@@ -8,17 +8,14 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-const config = {
-  allowedConnections: ['facebook', 'github', 'facebook'],
-};
+
 root.render(
   <React.StrictMode>
     <Auth0Provider
       domain='dev-lkdpc934.us.auth0.com'
-      clientId='lNqk6eBm53HpBUykPgzrEAMhd3vqSkr6'
+      clientId='lNqk6eBm53HpBUykPgzrEAMhd3vqSkr6' //tried .env but didnt work
       redirectUri={window.location.origin}
       baseURL='http://localhost:3000'
-      options={config}
     >
       <App />
     </Auth0Provider>
